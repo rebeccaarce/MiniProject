ba901601a04e63c157bd9f427c46700a9507e9be
sliu17 <7spencer7@gmail.com>
2018-09-13 15:13:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 var express = require('express');
-var router = express Router();
+var router = express.Router();
 
-router get('/', function(req,res,next) {res.render('index', {title: 'Members'});
+/* GET home page. */
+router.get('/', ensureAuthenticated, function(req, res, next) {
+  res.render('index', { title: 'Members' });
 });
+
+function ensureAuthenticated(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	}
+	res.redirect('/users/login');
+}
 
 module.exports = router;
